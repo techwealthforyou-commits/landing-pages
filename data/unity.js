e952fd8fcd925d3ced0259d0b821eef0fc578c24
techwealthforyou-commits <techwealthforyou@gmail.com>
2026-04-29 22:51:12 +0700

    Add files via upload

---
+++ data/unity.js
@@ -0,0 +1,68 @@
+window.DATA = {
+
+  hero: {
+    title: "Build Games Faster with Unity Assets",
+    desc: "Create mobile games, VR/AR apps, and 3D experiences without starting from scratch.",
+    text: "Visit",
+    link: "https://af.unity.com/sr/camref:1011l4gm2v",
+    image: "../assets/images/unity/hero.jpg", // 👈 ADD
+    project: "unity",
+    value: 3
+  },
+
+  content1: {
+    title: "Why Game Developers Struggle",
+    items: [
+      "Building games from scratch takes months",
+      "Lack of high-quality 2D/3D assets",
+      "No experience in coding or game engines",
+      "High development cost for assets and tools",
+      "Difficulty creating VR/AR or mobile games"
+    ]
+  },
+
+  content2: {
+    title: "How Unity Asset Store Solves This",
+    items: [
+      "Access thousands of ready-made 2D, 3D, animation, and UI assets instantly",
+      "Build games without deep coding knowledge",
+      "Create mobile games and apps faster",
+      "Reduce development time from months to days",
+      "Save costs with affordable tools"
+    ],
+    image: "../assets/images/unity/demo.jpg" // 👈 ADD
+  },
+
+  features: {
+    title: "What You Can Build with Unity",
+    desc: "Create games, apps, and experiences faster.",
+    items: [
+      " Build Mobile Games",
+      " Use Ready-Made Assets",
+      " No Coding Needed",
+      " Create VR / AR Apps",
+      " 3D Visualization",
+      " Simulation Apps"
+    ]
+  },
+
+  midCTA: {
+    title: "⚡ Limited-Time Unity Deals",
+    desc: "Get up to 95% OFF selected assets.",
+    text: "Grab Deals Now",
+    link: "https://af.unity.com/sr/camref:1011l4gm2v
+",
+    project: "unity",
+    value: 2
+  },
+
+  cta: {
+    title: "Start Building Your Game Today",
+    desc: "Stop wasting months building from scratch.",
+    text: "Browse Unity Asset Store",
+    link: "https://af.unity.com/sr/camref:1011l4gm2v",
+    project: "unity",
+    value: 1
+  }
+
+};
